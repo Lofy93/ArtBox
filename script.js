66357374322fc401b96146b060e0289037764cdd
Lofy <supercat2017@yahoo.com>
2024-08-18 15:17:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 let Pixels = document.getElementsByClassName("Pixels");
-let parent = document.getElementById("parent");
-let html = document.getElementById('html')
-let Timer = document.querySelector('.Timer')
-let color = document.querySelector('.color')
+let Box = document.getElementById("parent");
+let Color = document.querySelector('.color');
 let x = 2500;
-let Key;
-let interval;
-
-
 
 for (let i = 0; i < x; i++) {
-    parent.innerHTML += `<div class="Pixels"></div> `;
+    Box.innerHTML += `<div class="Pixels"></div> `;
 }
 
 for (let i = 0; i < x; i++) {
     Pixels[i].onclick = function () {
-        Pixels[i].style.background = color.value;
-    }};
-       
+        Pixels[i].style.background = Color.value;
+}};
