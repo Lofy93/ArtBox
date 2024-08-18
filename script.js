da6a8e51a756ef52b67039db57ee4aed8b71f9e2
Lofy <supercat2017@yahoo.com>
2024-08-18 15:26:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let Pixels = document.getElementsByClassName("Pixels");
-let Box = document.getElementById("Box");
-let Color = document.querySelector('.Color');
-let x = 2500;
+let Pixels = document.getElementsByClassName("Pixels")
+let Box = document.getElementById("Box")
+let Color = document.querySelector('.Color')
+let x = 2500
 
 for (let i = 0; i < x; i++) {
-    Box.innerHTML += `<div class="Pixels"></div> `;
+    Box.innerHTML += `<div class="Pixels"></div> `
 }
 
 for (let i = 0; i < x; i++) {
     Pixels[i].onclick = function () {
-    Pixels[i].style.background = Color.value;
-}};
+        Pixels[i].style.background = Color.value
+    }
+}
